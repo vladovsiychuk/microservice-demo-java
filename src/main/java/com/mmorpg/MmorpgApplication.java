bf805cc2be4bc6c8edbe701fe02d4329c52e083a
vladovsiychuk <vladyslav.ovsiychuk@gmail.com>
2024-10-05 14:27:21 +0300

    initial commit

---
+++ src/main/java/com/mmorpg/MmorpgApplication.java
@@ -0,0 +1,13 @@
+package com.mmorpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmorpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmorpgApplication.class, args);
+	}
+
+}
