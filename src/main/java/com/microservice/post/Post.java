f0b9c284d13bf96c858db45de31b3edd5d775652
vladovsiychuk <vladyslav.ovsiychuk@gmail.com>
2024-10-11 13:34:35 +0300

    fix get posts list

--- src/main/java/com/microservice/post/Post.java
+++ src/main/java/com/microservice/post/Post.java
@@ -4,13 +4,14 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.domain.Persistable;
 
 @Getter @Setter
-@AllArgsConstructor
+@AllArgsConstructor @NoArgsConstructor
 public class Post implements Persistable<UUID> {
     @Id private UUID id;
     private String content;
