3eee365e88bd42c902292d9f7109fb0382f5f8db
vladovsiychuk <vladyslav.ovsiychuk@gmail.com>
2024-10-11 19:48:47 +0300

    create controller test

---
+++ src/test/java/com/microservice/post/PostControllerTest.java
@@ -0,0 +1,36 @@
+package com.microservice.post;
+
+import org.junit.jupiter.api.Test;
+import static org.mockito.BDDMockito.given;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+@WebFluxTest(PostController.class) // Make sure you're using the right annotation for WebFlux
+class PostControllerTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @MockBean
+    private PostService postService; // Mock the PostService
+
+    @Test
+    void abc() {
+        Post post = Post.create(new PostCommand("hello")); // Create a mock Post object
+
+
+        given(this.postService.create(new PostCommand("hello"))).willReturn(Mono.just(post));
+
+        // Perform a POST request and assert the response status
+        webTestClient.post()
+            .uri("/v1/posts")
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(new PostCommand("hello")) // Send a JSON body with PostCommand
+            .exchange()
+            .expectStatus().isOk(); // Expect a 200 OK status
+    }
+}
