9c6f2e601d8ab8b0213edf6724c66ec49f4b5f96
vladovsiychuk <vladyslav.ovsiychuk@gmail.com>
2024-10-11 13:33:25 +0300

    fix saving with persistable interface

--- src/main/java/com/microservice/post/Post.java
+++ src/main/java/com/microservice/post/Post.java
@@ -1,20 +1,30 @@
 package com.microservice.post;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.domain.Persistable;
 
 @Getter @Setter
 @AllArgsConstructor
-public class Post {
-    @Id
-    private UUID id;
+public class Post implements Persistable<UUID> {
+    @Id private UUID id;
     private String content;
+    @Transient @JsonIgnore
+    private boolean isNew;
 
     public Post(PostCommand command) {
         id = UUID.randomUUID();
         content = command.content();
+        isNew = true;
+    }
+
+    @Override
+    public boolean isNew() {
+        return isNew;
     }
 }
