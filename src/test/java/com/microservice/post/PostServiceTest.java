216c882a191f2dcb6db4c74b25ab2b7c70c3a407
vladovsiychuk <vladyslav.ovsiychuk@gmail.com>
2024-10-11 18:54:26 +0300

    add error tests

--- src/test/java/com/microservice/post/PostServiceTest.java
+++ src/test/java/com/microservice/post/PostServiceTest.java
@@ -1,13 +1,18 @@
 package com.microservice.post;
 
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.mockito.ArgumentMatchers.any;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -21,16 +26,23 @@ class PostServiceTest {
     @Mock
     private PostRepository postRepository;
 
+    private MockedStatic<Post> postMockedStatic;
+
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
+        postMockedStatic = mockStatic(Post.class);
+    }
+
+    @AfterEach
+    void tearDown() {
+        postMockedStatic.close();
     }
 
     @Test
     void testCreate_ShouldCallSave() {
-        var postMockedStatic = mockStatic(Post.class);
         Post newPost = mock(Post.class);
-        PostCommand postCommand = mock(PostCommand.class); // Mock PostCommand if needed
+        PostCommand postCommand = mock(PostCommand.class);
 
         postMockedStatic.when(() -> Post.create(postCommand)).thenReturn(newPost);
         when(postRepository.save(any(Post.class))).thenReturn(Mono.just(newPost));
@@ -39,6 +51,37 @@ class PostServiceTest {
 
         assertNotNull(result);
 
-        verify(postRepository, times(1)).save(any(Post.class)); // Verify save was called once
+        verify(postRepository, times(1)).save(any(Post.class));
+    }
+
+    @Test
+    void testCreate_ShouldThrowAnErrorWhenRepositoryFailsTheSave() {
+        Post newPost = mock(Post.class);
+        PostCommand postCommand = mock(PostCommand.class); // Mock PostCommand if needed
+
+        postMockedStatic.when(() -> Post.create(postCommand)).thenReturn(newPost);
+        when(postRepository.save(any(Post.class))).thenReturn(Mono.error(new RuntimeException("Error")));
+
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+            postService.create(postCommand).block();
+        });
+
+        assertEquals("Error", exception.getMessage());
+    }
+
+    @Test
+    void testCreate_ShouldThrowAnErrorWhenPostModelFails() {
+        Post newPost = mock(Post.class);
+        PostCommand postCommand = mock(PostCommand.class);
+
+        postMockedStatic.when(() -> Post.create(postCommand)).thenThrow(new RuntimeException("Error"));
+        when(postRepository.save(any(Post.class))).thenReturn(Mono.just(newPost));
+
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+            postService.create(postCommand).block();
+        });
+
+        assertEquals("Error", exception.getMessage());
+        verify(postRepository, never()).save(any(Post.class));
     }
 }
