8ca770d4417232e8ac58c23acb27404924daa7ae
vladovsiychuk <vladyslav.ovsiychuk@gmail.com>
2024-10-15 12:02:22 +0300

    setup mongodb

--- src/main/java/com/microservice/post/Post.java
+++ src/main/java/com/microservice/post/Post.java
@@ -8,7 +8,9 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.domain.Persistable;
+import org.springframework.data.relational.core.mapping.Table;
 
+@Table
 @Data
 @AllArgsConstructor @NoArgsConstructor
 public class Post implements Persistable<UUID> {
