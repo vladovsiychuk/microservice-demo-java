4c9e3d42001f217fa5e370dc1d44ba8eb6966f99
vladovsiychuk <vladyslav.ovsiychuk@gmail.com>
2024-10-17 14:22:05 +0300

    add redis reactive dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+	implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
 	implementation("org.liquibase:liquibase-core")
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
 	runtimeOnly("org.postgresql:postgresql")
