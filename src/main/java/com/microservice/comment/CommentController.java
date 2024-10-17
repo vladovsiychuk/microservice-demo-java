9950b17d52cb5bdfc9d7a179021a20d8be1eb165
vladovsiychuk <vladyslav.ovsiychuk@gmail.com>
2024-10-17 11:26:26 +0300

    add comment update to CommentController

--- src/main/java/com/microservice/comment/CommentController.java
+++ src/main/java/com/microservice/comment/CommentController.java
@@ -4,21 +4,26 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/v1/posts/{id}/comment")
+@RequestMapping("/v1/posts/{postId}/comment")
 public class CommentController {
 
     @Autowired
     CommentService service;
 
     @PostMapping
-    public Mono<Comment> create(@RequestBody CommentCommand command, @PathVariable UUID id) {
-        var newCommand = new CommentCommand(command.content(), id);
-        return service.create(newCommand);
+    public Mono<Comment> create(@RequestBody CommentCommand command, @PathVariable UUID postId) {
+        return service.create(postId, command);
+    }
+
+    @PutMapping("/{commentId}")
+    public Mono<Comment> update(@RequestBody CommentCommand command, @PathVariable UUID commentId) {
+        return service.update(commentId, command);
     }
 }
